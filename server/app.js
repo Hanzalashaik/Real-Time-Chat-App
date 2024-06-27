bd5ce66f713fb32fa9a99a470b2d94d2ffe60b7c
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-06-27 14:57:02 +0530

    app.js updated

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { app, server } from "./socket/socket.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = config.get("PORT");
 const URL = config.get("URL");
 
@@ -25,6 +30,10 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 server.listen(PORT, (req, res) => {
   console.log(`Server is running at port ${PORT} 🚀`);
